9f34217bed4b1c248bf6d24d50c65d245a2382db
keith shannep <keithshannep@gmail.com>
2018-06-01 09:33:47 -0600

    Working on all fileds and dropdowns
    Not ready yet.

--- RealD Helpdesk/MainWindow.xaml.cs
+++ RealD Helpdesk/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace RealD_Helpdesk
 {
@@ -28,7 +29,46 @@ namespace RealD_Helpdesk
         //Email helpdesk Button.
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                // Create the Outlook application.
+                Outlook.Application oApp = new Outlook.Application();
 
+                // Create a new mail item.
+                Outlook.MailItem oMsg = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+
+                //add the body of the email             
+                oMsg.HTMLBody =
+                    "<Strong> Customer: </Strong> " + this.NameBox.Text;
+               
+                //Subject line
+                oMsg.Subject = " ";
+
+                // Add a recipient.
+                Outlook.Recipients oRecips = (Outlook.Recipients)oMsg.Recipients;
+
+                // Change the recipient in the next line if necessary.
+                Outlook.Recipient oRecip = (Outlook.Recipient)oRecips.Add("Kshannep@reald.com");
+                oRecip.Resolve();
+
+                // Send.
+                oMsg.Send();
+
+                // Clean up.
+                oRecip = null;
+                oRecips = null;
+                oMsg = null;
+                oApp = null;
+
+                // display submitted box
+                MessageBox.Show("Your ticket has been submitted!");
+
+                Close();
+
+            }//end of try block
+            catch (Exception)
+            {
+            }
         }
     }
 }
